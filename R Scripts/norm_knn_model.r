1c4d9eaaab4e910bbcadc13c29860fe43e55db49
SecurityCard <94642210+SecurityCard@users.noreply.github.com>
2021-12-05 13:22:19 -0600

    Update norm_knn_model.r

--- R Scripts/norm_knn_model.r
+++ R Scripts/norm_knn_model.r
@@ -1,5 +1,4 @@
 library(class)
-library(caTools)
 
 normalize <- function(x) {
 return((x - min(x)) / (max(x) - min(x))) }
@@ -7,11 +6,17 @@ return((x - min(x)) / (max(x) - min(x))) }
 data <- read.csv(file.choose(), header = TRUE)
 data <- data[1:748, c(1:2,4:5)]
 data_n <- as.data.frame(lapply(data[,c(1,2,3,4)], normalize))
+colnames(data_n) <- c("Recency", "Frequency", "Time", "donatedMar07")
 
-train <- data_n[1:598,]
-test <- data_n[599:748,]
-train_target <- data_n[1:598, 4]
-test_target <- data_n[599:748, 4]
+n.points <- 748
+sampling.rate <- 0.8
+num.test.set.labels <- n.points * (1 - sampling.rate)
+training <- sample(1:n.points, sampling.rate * n.points, replace = FALSE)
+train <- subset(data_n[training, ], select = c(Recency, Frequency, Time))
+testing <- setdiff(1:n.points, training)
+test <- subset(data_n[testing, ], select = c(Recency, Frequency, Time))
+train_target <- data_n$donatedMar07[training]
+test_target <- data_n$donatedMar07[testing]
 
 for(k in 17:37) {
     print(k)
